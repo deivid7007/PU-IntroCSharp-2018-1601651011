db236791826a21f73562cd3e0b766d0f1a0eaa01
Deivid Mladenov <deividmladenov@outlook.com>
2018-09-11 21:25:10 +0300

    ллл

---
+++ PUIntroHomeworks/AgeAfter10Years/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
