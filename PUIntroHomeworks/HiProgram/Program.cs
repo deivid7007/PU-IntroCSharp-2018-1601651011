3b04a5efb3ed9b577618fb428a8fe7eae804ef59
Deivid Mladenov <deividmladenov@outlook.com>
2018-09-23 16:50:28 +0300

    .........

--- PUIntroHomeworks/HiProgram/Program.cs
+++ PUIntroHomeworks/HiProgram/Program.cs
@@ -7,6 +7,7 @@ namespace HiProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(".....");
         }
     }
 }
--- PUIntroHomeworks/MyUniqueNumber/Program.cs
+++ PUIntroHomeworks/MyUniqueNumber/Program.cs
@@ -7,6 +7,7 @@ namespace MyUniqueNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("sasdadad");
         }
     }
 }
