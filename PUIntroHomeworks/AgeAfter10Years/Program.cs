d189e235f082c1ce509e3168b87c857e2f2a5da4
Deivid Mladenov <deividmladenov@outlook.com>
2018-09-23 16:42:52 +0300

    .....

---
+++ PUIntroHomeworks/AgeAfter10Years/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is cw(tab,tab)
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PUIntroHomeworks/MyUniqueNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
